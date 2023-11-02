9e8c4369d10cb518c11bdc6b9d36c258e98ecbce
Mashhura <mashhura2020@gmail.com>
2023-11-03 03:34:20 +0500

    modified files online store,shopping,news,added onlinecard file

--- src/Components/Shopping/Shopping.jsx
+++ src/Components/Shopping/Shopping.jsx
@@ -3,8 +3,14 @@ import './Shopping.scss'
 
 const Shopping = () => {
   return (
-    <div>
-      
+    <div className='shopping '>
+      <div className='container'>
+
+        {/* Title here */}
+        <p>Lorem ipsum dolor sit amet, <a href="">consectetur adipiscing elit</a> . Suspendisse varius enim in eros elementum.</p>
+
+        {/* Button comes here */}
+     </div>
     </div>
   )
 }
