470ea34ef50ccd148bb3c44779c1ab79c86c82c0
Kurbanov Jakhongir <asd@asd.ws>
2023-11-03 12:41:30 +0500

    gulnoza_changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
-import Header from './Components/Layout/Header.jsx'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-    <Header />
+     
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Layout/Header.jsx
+++ src/Components/Layout/Header.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
 import './Header.scss'
+import  Logo from "../../assets/img/logo.png"
+
+import { useRef } from "react";
+import { FaBars, FaTimes ,FaShoppingCart} from "react-icons/fa";
 
 const Header = () => {
+  const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle("responsive_nav")}
+      
+  
   return (
-    <div>
-      <h1>hello world</h1>
-      <h3>salom</h3>
-    </div>
+    
+     <header className="header">
+      <a href="#" className="header-logo">    <img src={Logo}/>  </a>
+
+      <div className="menu"  ref={navRef}>
+        <ul className='menu-list'>
+          <li><a href="#" className='menu-item-link'> Menu</a></li>
+          <li><a href="#" className='menu-item-link'> About</a></li>
+          <li><a href="#" className='menu-item-link'> Shop</a></li>
+          <li><a href="#" className='menu-item-link'> Contact</a></li>
+          <button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<FaTimes />
+				</button>
+        </ul>
+        <a href="#" className='menu-item-link-cart' >  <FaShoppingCart/>Cart</a>
+      </div>
+      <button
+				className="nav-btn"
+				onClick={showNavbar}>
+				<FaBars />
+			</button>
+     </header>
+    
   )
 }
 
