2fcb3700b837543b87125d96b568361a8b86367b
Sevara Obidova <sevaraobidova505@gmail.com>
2023-11-04 13:37:52 +0500

    app

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,22 @@ import './App.css'
 import Header from './Components/Layout/Header.jsx'
 import Showcase from './Components/Showcase/Showcase'
 import Product from './Components/Product/Product'
+import OnlineStore from './Components/OnlineStore/OnlineStore'
+import Shopping from './Components/Shopping/Shopping'
+import News from './Components/News/News'
+import Footer from './Components/Layout/Footer'
 
 function App() {
 
   return (
     <>
+    <Header />
+    <Showcase />
+    <Product />
+    <OnlineStore />
+    <Shopping />
+    <News />
+    <Footer />
     </>
   )
 }
